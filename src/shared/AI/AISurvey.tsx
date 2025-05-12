a143ac330475668b90d2f229ecfced1bee27f3d6
Hyeonjin <oboki2001@naver.com>
2025-05-12 19:16:45 +0900

    [refactor] 컴포넌트에 따라 스크롤바 스타일링을 다르게 하기 위해, 커스텀 스크롤바 클래스명 추가

--- src/shared/AI/AISurvey.tsx
+++ src/shared/AI/AISurvey.tsx
@@ -7,14 +7,16 @@ const AISurvey = () => {
   const [isClickedEssential, setIsClickedEssential] = useState<boolean>(true);
 
   return (
-    <div className="rounded-[10px] bg-white px-[15px] pt-[10px] pb-[20px] [box-shadow:3px_3px_10px_rgba(0,0,0,0.25)] w-[357px] h-[500px] fixed bottom-[40px] right-[85px] overflow-y-auto scrollbar-custom">
-      <SurveyHeader
-        isClickedEssential={isClickedEssential}
-        setIsClickedEssential={setIsClickedEssential}
-      />
-      <section className="mt-[20px]">
-        {isClickedEssential ? <EssentialQuestions /> : <OptionalQuestions />}
-      </section>
+    <div className="scrollbar-partial-rounded fixed bottom-[40px] right-[85px]">
+      <div className="rounded-[10px] bg-white px-[15px] pt-[10px] pb-[20px] [box-shadow:0px_0px_20px_rgba(0,0,0,0.6)] w-[357px] h-[500px] overflow-y-auto scrollbar-custom">
+        <SurveyHeader
+          isClickedEssential={isClickedEssential}
+          setIsClickedEssential={setIsClickedEssential}
+        />
+        <section className="mt-[20px]">
+          {isClickedEssential ? <EssentialQuestions /> : <OptionalQuestions />}
+        </section>
+      </div>
     </div>
   );
 };
