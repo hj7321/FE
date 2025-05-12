bc0b0c9c6ce5584efcb5d4891948b6a312f926b8
Hyeonjin <oboki2001@naver.com>
2025-05-12 19:29:51 +0900

    [feat & style] - 페이지에 머물러서 새로고침 시에는 장바구니 목록이 초기화되지 않고, 페이지를 떠날 시에는 장바구니 목록이 초기화되도록 useEffect 구현 - useParams를 통해 place에 따라 국기, 나라/지역 이름 다르게 나오도록 구현 - 검색바, 장소 카드 렌더링

--- src/pages/place_exploration/PlaceExplorationPage.tsx
+++ src/pages/place_exploration/PlaceExplorationPage.tsx
@@ -1,5 +1,51 @@
+import { useParams } from "react-router";
+import FavoritePlaceListButton from "../../components/button/FavoritePlaceListButton";
+import PlaceCard from "../../components/card/PlaceCard";
+import SearchBar from "../../components/SearchBar";
+import { usefavoriteListStore } from "../../stores/favoriteList.store";
+import { useEffect, useRef } from "react";
+
 const PlaceExplorationPage = () => {
-  return <div>장소 탐색 페이지</div>;
+  const { place } = useParams();
+  const country = place?.split(" ")[0];
+  const { resetFavoriteList } = usefavoriteListStore();
+  const currentPathRef = useRef(location.pathname);
+
+  useEffect(() => {
+    return () => {
+      if (location.pathname !== currentPathRef.current) resetFavoriteList();
+    };
+  }, [location.pathname, resetFavoriteList]);
+
+  return (
+    <div className="px-[100px] py-[10px]">
+      <section className="flex gap-[8px] items-center">
+        <img
+          src={`/images/flags/${country}.svg`}
+          alt={country}
+          className="text-[10px] rounded-[6px] aspect-[3/2] h-[35px]"
+        />
+        <h1 className="font-extrabold text-[25px]">{place}</h1>
+      </section>
+      <section className="my-[12px]">
+        <h1 className="text-[22px] font-bold">인기 장소 TOP 20</h1>
+        <div>슬라이드 부분</div>
+      </section>
+      <section className="flex flex-col gap-[25px]">
+        <div className="flex flex-col gap-[8px] items-center">
+          <p className="font-bold text-[25px] text-center">장소 찾기</p>
+          <SearchBar placeholder={`${country} 내 장소를`} placeExploration />
+        </div>
+        <div className="py-[20px] flex flex-wrap gap-x-[20px] gap-y-[30px]">
+          <PlaceCard cardImg="/images/cities/서울.jpg" cardName="경복궁" />
+          <PlaceCard cardImg="/images/cities/서울.jpg" cardName="경복궁" />
+          <PlaceCard cardImg="/images/cities/서울.jpg" cardName="경복궁" />
+          <PlaceCard cardImg="/images/default.png" cardName="텍스트" />
+        </div>
+      </section>
+      <FavoritePlaceListButton />
+    </div>
+  );
 };
 
 export default PlaceExplorationPage;
