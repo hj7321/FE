924d53b32af43705d706e22bf1c2c8ed3941c61e
Hyeonjin <oboki2001@naver.com>
2025-05-12 19:25:09 +0900

    [feat & style] - 짧은 input 필드(인증번호 입력)를 위해 short prop 추가 - short일 때 width 좀 더 짧게 구현

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -1,14 +1,28 @@
+import clsx from "clsx";
+
 interface AuthInputProps {
   label: string;
   type: string;
+  short?: boolean;
 }
 
-const AuthInput = ({ label, type }: AuthInputProps) => {
+const AuthInput = ({ label, type, short }: AuthInputProps) => {
   return (
     <label>
-      <p className="text-[13px] mb-[2px] text-[#333333]">{label}</p>
-      <div className="px-[15px] w-[250px] h-[47px] bg-white rounded-[4px] [box-shadow:2px_2px_5px_rgba(0,0,0,0.25)] flex items-center">
-        <input type={type} className="outline-none w-[200px]" />
+      <p className="text-[12px] mb-[1px] text-[#333333]">{label}</p>
+      <div
+        className={clsx(
+          "px-[15px] h-[45px] bg-white rounded-[4px] [box-shadow:2px_2px_5px_rgba(0,0,0,0.25)] flex items-center",
+          short ? "w-[130px]" : "w-[250px]"
+        )}
+      >
+        <input
+          type={type}
+          className={clsx(
+            "outline-none text-[15px]",
+            short ? "w-[90px]" : "w-[200px]"
+          )}
+        />
       </div>
     </label>
   );
