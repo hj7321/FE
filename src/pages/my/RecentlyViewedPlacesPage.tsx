91b9dd1d450e7d16ade9e078f7fa6cbbba9b6724
Hyeonjin <oboki2001@naver.com>
2025-05-12 19:27:29 +0900

    [style] 최근에 본 장소 카드 렌더링

--- src/pages/my/RecentlyViewedPlacesPage.tsx
+++ src/pages/my/RecentlyViewedPlacesPage.tsx
@@ -1,5 +1,63 @@
+import RecentPlaceCard from "../../components/card/RecentPlaceCard";
+
 const RecentlyViewedPlacesPage = () => {
-  return <div>최근에 본 장소 페이지</div>;
+  return (
+    <div className="px-[100px] py-[10px] flex flex-col">
+      <h1 className="font-bold text-[22px]">최근에 본 장소📍</h1>
+      <div className="mt-[7px] flex flex-wrap gap-x-[20px] gap-y-[30px]">
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+        <RecentPlaceCard
+          cardImg="/images/default.png"
+          countryName="이탈리아 로마"
+          placeName="성 베드로 대성당"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RecentlyViewedPlacesPage;
