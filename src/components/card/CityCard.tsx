9173c307618924fc9615557a812080e22a409545
Hyeonjin <oboki2001@naver.com>
2025-05-12 18:56:17 +0900

    [add & style] 지역 카드 UI 구현

---
+++ src/components/card/CityCard.tsx
@@ -0,0 +1,32 @@
+import { useModalStore } from "../../stores/modal.store";
+import CityModal from "../modal/CityModal";
+
+interface CityCardProps {
+  cardImg: string;
+  cardName: string;
+}
+
+const CityCard = ({ cardImg, cardName }: CityCardProps) => {
+  const { openModal } = useModalStore();
+
+  const handleOpenModal = () => {
+    openModal(<CityModal cardName={cardName} cardImg={cardImg} />);
+  };
+
+  return (
+    <div
+      onClick={handleOpenModal}
+      className="flex flex-col gap-[3px] p-[10px] bg-white w-[303.5px] h-fit rounded-[8px] [box-shadow:4px_8px_4px_rgba(0,0,0,0.1)] hover:cursor-pointer duration-300 hover:scale-108 overflow-hidden"
+    >
+      <img
+        src={cardImg}
+        alt={cardName}
+        className="rounded-[4px] h-[200px] object-cover text-[12px]"
+      />
+
+      <p>{cardName}</p>
+    </div>
+  );
+};
+
+export default CityCard;
