c68caf403e2d44701427df1ad7b9edc9e61a4fd0
Hyeonjin <oboki2001@naver.com>
2025-05-12 19:18:55 +0900

    [style] - 모달 뒷배경 UI 컴포넌트 추가 - auth 페이지에서 헤더, 푸터, AI 버튼 제거

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -1,21 +1,35 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Footer from "./Footer";
 import Header from "./Header";
 import ScrollToTop from "./ScrollToTop";
 import TopButton from "./TopButton";
 import AIButton from "./AI/AIButton";
+import clsx from "clsx";
+import ModalBackground from "../components/modal/ModalBackground";
 
 const Layout = () => {
+  const { pathname }: { pathname: string } = useLocation();
+  const authPath = ["/login", "/sign-up"];
+  const hideLayout = authPath.some((path) => pathname.includes(path));
+
   return (
     <>
-      <Header />
-      <main className="bg-[#EDEDED] h-[600px]">
+      <ModalBackground />
+      {!hideLayout && <Header />}
+      <main
+        className={clsx(
+          "bg-[#EDEDED]",
+          hideLayout ? "min-h-screen" : "min-h-[600px]"
+        )}
+      >
         <ScrollToTop />
         <Outlet />
       </main>
-      <TopButton />
-      <AIButton />
-      <Footer />
+      <TopButton
+        isExistedLikeButton={pathname.startsWith("/place-exploration")}
+      />
+      {!hideLayout && <AIButton />}
+      {!hideLayout && <Footer />}
     </>
   );
 };
