0d9bf0d8b8e709c9cae80584aa639e552b915830
Hyeonjin <oboki2001@naver.com>
2025-05-17 23:24:07 +0900

    [style] 스타일 일부 변경, 비밀번호 입력란에 눈(eye) 아이콘 삽입

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -12,7 +12,7 @@ const AuthInput = ({ label, type, short }: AuthInputProps) => {
       <p className="text-[12px] mb-[1px] text-[#333333]">{label}</p>
       <div
         className={clsx(
-          "px-[15px] h-[45px] bg-white rounded-[4px] [box-shadow:2px_2px_5px_rgba(0,0,0,0.25)] flex items-center",
+          "px-[15px] h-[45px] bg-white rounded-[4px] [box-shadow:2px_2px_5px_rgba(0,0,0,0.25)] flex items-center justify-between",
           short ? "w-[130px]" : "w-[250px]"
         )}
       >
@@ -20,9 +20,16 @@ const AuthInput = ({ label, type, short }: AuthInputProps) => {
           type={type}
           className={clsx(
             "outline-none text-[15px]",
-            short ? "w-[90px]" : "w-[200px]"
+            short ? "w-[90px]" : "w-[190px]"
           )}
         />
+        {type === "password" && (
+          <img
+            src="/images/eye-open.svg"
+            alt="eye"
+            className="text-[10px] w-[20px] hover:cursor-pointer"
+          />
+        )}
       </div>
     </label>
   );
