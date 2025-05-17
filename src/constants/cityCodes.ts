2a305fba652c5e2add2f8c83b7864b181cd6526d
Hyeonjin <oboki2001@naver.com>
2025-05-17 23:29:43 +0900

    [add] 항공권 조회를 위한 도시 코드 객체 생성

---
+++ src/constants/cityCodes.ts
@@ -0,0 +1,44 @@
+export const CITY_TO_SKYSCANNER_CODE: { [cityName: string]: string } = {
+  // 대한민국
+  서울: "sela",
+  부산: "pus",
+  제주: "cju",
+  경주: "usna",
+  강릉: "ynya",
+  전주: "kuwa",
+  수원: "gmpa",
+
+  // 일본
+  오사카: "osa",
+  도쿄: "tyoa",
+  후쿠오카: "fuk",
+  삿포로: "spka",
+
+  // 중국
+  베이징: "bjsa",
+  상하이: "csha",
+
+  // 태국
+  방콕: "bkkt",
+  치앙마이: "cnx",
+  코사무이: "usm",
+
+  // 미국
+  뉴욕: "nyca",
+  하와이: "hnl",
+  괌: "gum",
+  로스앤젤레스: "laxa",
+  샌프란시스코: "sfo",
+  사이판: "spn",
+
+  // 이탈리아
+  로마: "rome",
+  피렌체: "flr",
+  베네치아: "veni",
+  밀라노: "mila",
+  나폴리: "nap",
+
+  // 스페인
+  바르셀로나: "bcn",
+  마드리드: "mad",
+};
