af2ef7f8c349e7823f182f7e5c55fcfb757a8670
Hyeonjin <oboki2001@naver.com>
2025-05-20 10:45:14 +0900

    [feat & style] 로고에 마우스를 댔을 때 그림자 효과가 이미지, 글자에 같이 나타나도록 구현

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,32 +1,54 @@
 import { Link } from "react-router";
+import { useAuthStore } from "../stores/auth.store";
+import { useState } from "react";
+import clsx from "clsx";
 
 const Header = () => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const isLogin = useAuthStore((state) => state.isLogin);
+  const logout = useAuthStore((state) => state.logout);
+
   return (
     <header className="px-[100px] py-[10px] flex items-center justify-between">
       <Link
         to="/"
         className="flex gap-[10px] transition-transform items-center"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
         <img
           src="/images/logo.svg"
           alt="logo"
-          className="[filter:drop-shadow(5px_5px_2px_rgba(0,0,0,0.25))] h-[53px] aspect-[4/5] hover:[filter:drop-shadow(5px_5px_2px_rgba(0,0,0,0.5))]"
+          className={clsx(
+            "[filter:drop-shadow(5px_5px_2px_rgba(0,0,0,0.25))] h-[53px] aspect-[4/5]",
+            isHovered && "![filter:drop-shadow(5px_5px_2px_rgba(0,0,0,0.4))]"
+          )}
         />
-        <p className="font-bold text-[35px] [text-shadow:3px_3px_5px_rgba(0,0,0,0.25)] hover:[text-shadow:3px_3px_5px_rgba(0,0,0,0.5)]">
+        <p
+          className={clsx(
+            "font-bold text-[35px] [text-shadow:3px_3px_5px_rgba(0,0,0,0.25)]",
+            isHovered && "![text-shadow:3px_3px_5px_rgba(0,0,0,0.4)]"
+          )}
+        >
           Tranner
         </p>
       </Link>
       <div className="flex gap-[45px]">
-        {/* 비로그인 시 */}
-        <Link to="/login" className="hover:font-bold">
-          로그인
-        </Link>
-        <Link to="/sign-up" className="hover:font-bold">
-          회원가입
-        </Link>
-        {/* 로그인 시 */}
-        {/* <Link to="/my">내 정보</Link>
-        <button>로그아웃</button> */}
+        {isLogin ? (
+          <>
+            <Link to="/my">내 정보</Link>
+            <button onClick={() => logout()}>로그아웃</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className="hover:font-bold">
+              로그인
+            </Link>
+            <Link to="/sign-up" className="hover:font-bold">
+              회원가입
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
