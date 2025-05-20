117c97f77f2309c1c342c40e4c410861f593fdc2
Hyeonjin <oboki2001@naver.com>
2025-05-20 10:45:49 +0900

    [feat] 메인 페이지 검색 기능 구현 중

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CountryButton from "../../components/button/CountryButton";
 import SearchBar from "../../components/SearchBar";
 import CityCard from "../../components/card/CityCard";
 import { COUNTRY_CITY } from "../../constants/countries";
 
 const MainPage = () => {
-  const [clickedCountry, setClickedCountry] = useState<string>("대한민국");
+  const [clickedCountry, setClickedCountry] = useState<string>("전체");
+  const [inputValue, setInputValue] = useState<string | null>(null);
+  const [searchResult, setSearchResult] = useState<string[]>([]);
+
+  console.log(inputValue);
+  console.log(searchResult);
+
+  useEffect(() => {
+    if (inputValue === null) {
+      setSearchResult([]);
+      return;
+    }
+    Object.keys(COUNTRY_CITY).forEach((country) => {
+      if (country.includes(inputValue)) {
+        setClickedCountry("전체");
+        setSearchResult((prev) => [...prev, ...COUNTRY_CITY[country]]);
+      }
+    });
+    Object.values(COUNTRY_CITY).forEach((cities) => {
+      cities.forEach((city) => {
+        if (city.includes(inputValue)) {
+          setClickedCountry("전체");
+          setSearchResult((prev) => [...prev, city]);
+        }
+      });
+    });
+  }, [inputValue]);
 
   return (
     <>
@@ -17,11 +43,23 @@ const MainPage = () => {
           <p className="font-bold text-[25px] text-center text-white [text-shadow:2px_2px_10px_rgba(0,0,0)]">
             여행지 찾기
           </p>
-          <SearchBar placeholder="원하는 여행지를" main />
+          <SearchBar
+            placeholder="원하는 여행지를"
+            main
+            inputValue={inputValue}
+            setInputValue={setInputValue}
+          />
         </div>
       </article>
       <section className="px-[100px] pt-[15px]">
         <div className="flex flex-wrap gap-[12px]">
+          <CountryButton
+            isSelected={clickedCountry === "전체"}
+            clickedCountry={clickedCountry}
+            onClicked={() => setClickedCountry("전체")}
+          >
+            전체
+          </CountryButton>
           {Object.keys(COUNTRY_CITY).map((country) => (
             <CountryButton
               key={country}
@@ -34,13 +72,32 @@ const MainPage = () => {
           ))}
         </div>
         <div className="py-[20px] flex flex-wrap gap-x-[35px] gap-y-[30px]">
-          {COUNTRY_CITY[clickedCountry].map((city) => (
-            <CityCard
-              key={city}
-              cardImg={`/images/cities/${city}.jpg`}
-              cardName={`${clickedCountry} ${city}`}
-            />
-          ))}
+          {inputValue !== null &&
+            searchResult.length > 0 &&
+            searchResult.map((result) => (
+              <CityCard
+                key={result}
+                cardImg={`/images/cities/${result}.jpg`}
+                cardName={result}
+              />
+            ))}
+          {clickedCountry === "전체" && inputValue === null
+            ? Object.entries(COUNTRY_CITY).map(([country, cities]) =>
+                cities.map((city) => (
+                  <CityCard
+                    key={city}
+                    cardImg={`/images/cities/${city}.jpg`}
+                    cardName={`${country} ${city}`}
+                  />
+                ))
+              )
+            : COUNTRY_CITY[clickedCountry].map((city) => (
+                <CityCard
+                  key={city}
+                  cardImg={`/images/cities/${city}.jpg`}
+                  cardName={`${clickedCountry} ${city}`}
+                />
+              ))}
         </div>
       </section>
     </>
