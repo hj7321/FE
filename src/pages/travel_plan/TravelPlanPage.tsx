6655794b48e5fcc9c43161d0b58910264e2bfcbf
Hyeonjin <oboki2001@naver.com>
2025-05-29 23:25:25 +0900

    [feat] 여행 계획 페이지에 처음 들어갈 때 달력 모달이 렌더링되도록 설정

--- src/pages/travel_plan/TravelPlanPage.tsx
+++ src/pages/travel_plan/TravelPlanPage.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import PlaceListScreen from "../../components/screen/PlaceListScreen";
 import ScheduleScreen from "../../components/screen/ScheduleScreen";
+import { useModalStore } from "../../stores/modal.store";
+import Calendar from "../../components/Calendar";
 
 const TravelPlanPage = () => {
+  const openModal = useModalStore((state) => state.openModal);
+
+  useEffect(() => {
+    openModal(<Calendar />);
+  }, [openModal]);
+
   return (
     <div className="flex">
       <ScheduleScreen />
