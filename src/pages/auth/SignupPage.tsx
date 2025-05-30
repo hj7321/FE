2df7af1aa4927a26f35e3618f8efeea39347b0d6
Hyeonjin <oboki2001@naver.com>
2025-05-31 00:16:04 +0900

    [style] 스타일 수정

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -2,7 +2,7 @@ import AuthForm from "../../components/auth/AuthForm";
 
 const SignupPage = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center">
       <AuthForm pageName="signup" />
     </div>
   );
