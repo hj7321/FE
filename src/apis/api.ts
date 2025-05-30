1f07cca3e977028f3601b013f33932b5120b555b
Hyeonjin <oboki2001@naver.com>
2025-05-30 23:59:56 +0900

    [chore] baseURL을 IP에서 도메인(api.tranner.com)으로 변경

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://3.35.102.134:8080/api";
+const BASE_URL = "https://api.tranner.com/api";
 
 export const api = axios.create({
   baseURL: BASE_URL,
