fb1c488ada3e8bdd9668bbee574adb515a7dc47a
Hyeonjin <oboki2001@naver.com>
2025-06-02 07:18:16 +0900

    [feat] - 같은 날짜 재선택 시 해제 - 오늘 날짜에 "오늘" 문자 표시 - 선택 완료 버튼 추가

--- src/components/modal/CalendarModal.tsx
+++ src/components/modal/CalendarModal.tsx
@@ -1,30 +1,60 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../../styles/calendar.css";
-import customKo from "../../utils/localeKoCustom";
+import { useDateStore } from "../../stores/date.store";
+import { useModalStore } from "../../stores/modal.store";
+import { isSameDay } from "date-fns";
+import { ko } from "date-fns/locale";
 
 const CalendarModal = () => {
+  const [calendarFocusDate, setCalendarFocusDate] = useState(new Date()); // 달력 기준 날짜
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
+  const lastSelectedRef = useRef<Date | null>(null); // 마지막 클릭 날짜 기억
   const today: Date = new Date();
-  const maxDate: Date = startDate
-    ? new Date(startDate.getTime() + 14 * 24 * 60 * 60 * 1000)
-    : new Date(today.getTime() + 14 * 24 * 60 * 60 * 1000);
+  const maxDate: Date | undefined = startDate
+    ? new Date(startDate.getTime() + 13 * 24 * 60 * 60 * 1000)
+    : undefined;
 
-  const handleChange = (dates: [Date | null, Date | null]) => {
+  const setDates = useDateStore((state) => state.setDates);
+  const closeModal = useModalStore((state) => state.closeModal);
+
+  const handleDateChange = (dates: [Date | null, Date | null]) => {
     const [start, end] = dates;
+
+    // 같은 날짜 다시 클릭 → 선택 해제
+    if (
+      start &&
+      !end &&
+      lastSelectedRef.current &&
+      start.getTime() === lastSelectedRef.current.getTime()
+    ) {
+      lastSelectedRef.current = null;
+      setStartDate(null);
+      setEndDate(null);
+      return;
+    }
+
+    // 새로운 날짜 선택
+    lastSelectedRef.current = start;
     setStartDate(start);
     setEndDate(end);
+  };
+
+  const handleCompleteTravelPeriod = () => {
+    if (!startDate || !endDate) return alert("여행 기간을 선택해주세요.");
+    setDates(startDate, endDate);
+    closeModal();
   };
 
   return (
     <div
       onClick={(e) => e.stopPropagation()}
-      className="z-[9999] w-[1000px] h-[600px] rounded-[6px] bg-white flex flex-col gap-[5px] items-center justify-between py-[25px] px-[10px]"
+      className="z-[9999] w-[1000px] h-[600px] rounded-[6px] bg-white flex flex-col items-center justify-between py-[25px] px-[10px]"
     >
       <h2 className="font-bold text-[26px]">여행 기간을 선택해주세요.</h2>
-      <div className="text-center text-[14px] text-[#959595] mt-[5px]">
+      <div className="text-center text-[14px] mt-[-5px] text-[#959595]">
         <p>
           여행 기간은 <b>최대 14일</b>까지 선택 가능합니다.
         </p>
@@ -35,20 +65,52 @@ const CalendarModal = () => {
       </div>
       {/* <div className="border w-[900px]"> */}
       <DatePicker
-        locale={customKo} // 한국어 적용
+        locale={ko}
         monthsShown={2} // 달력 두 달 치 렌더링
-        selected={today} // 현재 선택된 시작 날짜를 지정
-        onChange={handleChange} // 날짜가 선택될 때 실행되는 콜백 함수 (start, end)
+        selected={startDate ?? undefined} //  선택된 날짜는 표시되게
+        key={(startDate?.toString() ?? "") + (endDate?.toString() ?? "")}
+        highlightDates={[]} // 강조된 날짜 비움
         startDate={startDate} // 범위 선택 시 시작 날짜 지정
         endDate={endDate} // 범위 선택 시 종료 날짜 지정
+        openToDate={calendarFocusDate} // 달력 포커스 수동 유지
+        onChange={handleDateChange}
         selectsRange // 범위(시작~종료) 선택을 활성화
         minDate={today} // 오늘 이전 날짜는 비활성화(선택 불가)
         maxDate={maxDate} // 14일 제한
         isClearable // 입력된 날짜를 X 버튼으로 지울 수 있도록 설정
         inline // 달력을 팝업이 아닌 고정형(인라인)으로 표시
+        onMonthChange={(date) => setCalendarFocusDate(date)}
+        // renderCustomHeader={(props) => <CalendarHeader {...props} />}
+        dayClassName={(date) => {
+          const isSunday = date.getDay() === 0;
+          const isOutOfRange = maxDate && date > maxDate;
+
+          if (isSunday && isOutOfRange) return "custom-sunday-disabled"; // 회색
+          if (isSunday) return "custom-sunday"; // 빨간색
+
+          return "";
+        }}
+        renderDayContents={(day, date) => {
+          const isToday = isSameDay(date!, new Date());
+          const isTodaySelected =
+            (startDate && isSameDay(date!, startDate)) ||
+            (endDate && isSameDay(date!, endDate));
+
+          return (
+            <div className="relative text-center">
+              <div>{day}</div>
+              {isToday && !isTodaySelected && (
+                <div className="text-[11px] mt-[-18px] text-gray-600">오늘</div>
+              )}
+            </div>
+          );
+        }}
       />
       {/* </div> */}
-      <button className="w-[130px] h-[40px] bg-common hover:bg-selected hover:cursor-pointer text-white text-[14px] rounded-[4px]">
+      <button
+        onClick={handleCompleteTravelPeriod}
+        className="w-[130px] h-[40px] bg-common hover:bg-selected hover:cursor-pointer text-white text-[14px] rounded-[4px]"
+      >
         날짜 선택 완료
       </button>
     </div>
