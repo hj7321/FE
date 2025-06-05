15d85b980239f23b4afaa735801ea6735857efe4
Hyeonjin <oboki2001@naver.com>
2025-06-06 02:50:37 +0900

    [feat] 장소 목록 요청/응답 및 상세 정보 타입 정의

--- src/types/place.type.ts
+++ src/types/place.type.ts
@@ -1,10 +1,13 @@
-export type Place = {
+export type PlaceId = {
   placeId: string;
+};
+
+export type Place = PlaceId & {
   placeName: string;
-  placeType: string;
+  placeType?: string;
   address: string;
-  latitude: number;
-  longitude: number;
+  latitude?: number;
+  longitude?: number;
 };
 
 export type CountryAndRegion = {
@@ -18,4 +21,34 @@ export type InsertBasketDataType = CountryAndRegion & {
 
 export type DeleteBasketDataType = CountryAndRegion & {
   placeId: string[];
+};
+
+export type ReadPlaceListRequestType = CountryAndRegion & {
+  pageToken: string | null;
+};
+
+export type Places = PlaceId & {
+  placeName: string;
+  placeType?: string;
+  photo?: string | null;
+};
+
+export type ReadPlaceListResponse = {
+  places: Places[];
+  pageToken?: string;
+};
+
+export type ReadPlaceDetailResponse = PlaceId & {
+  name: string;
+  address: string;
+  latitude?: number;
+  longitude?: number;
+  summary?: string;
+  openingHours?: {
+    weekdayText?: string[];
+  };
+  phoneNumber?: string;
+  url?: string;
+  placeType?: string;
+  photoUrl?: string | null;
 };
