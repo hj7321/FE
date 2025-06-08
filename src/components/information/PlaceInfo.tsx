04aae703b5da9e933c2fb24bbed08a936c95dadc
Hyeonjin <oboki2001@naver.com>
2025-06-09 02:04:34 +0900

    [refactor] PlaceInfo 컴포넌트를 placeData 기반으로 구조 개선

--- src/components/information/PlaceInfo.tsx
+++ src/components/information/PlaceInfo.tsx
@@ -1,12 +1,32 @@
+import { ReadPlaceDetailResponse } from "../../types/place.type";
+
 interface PlaceInfoProps {
-  info: string;
+  placeData: ReadPlaceDetailResponse;
 }
 
-const PlaceInfo = ({ info }: PlaceInfoProps) => {
+const PlaceInfo = ({ placeData }: PlaceInfoProps) => {
   return (
-    <p className="text-[14.5px] overflow-y-auto scrollbar-custom pr-[10px]">
-      {info}
-    </p>
+    <div className="text-[14.5px] flex flex-col gap-[10px]">
+      {placeData.phoneNumber && (
+        <div className="flex gap-[6px]">
+          <img src="/images/phone.svg" alt="phone" />
+          <p>{placeData.phoneNumber}</p>
+        </div>
+      )}
+      {placeData.openingHours && (
+        <div className="flex gap-[7px] items-start">
+          <img src="/images/time.svg" alt="time" className="mt-[4px]" />
+          <div className="flex flex-col">
+            {placeData.openingHours?.weekdayText?.map((text) => (
+              <p key={text}>{text}</p>
+            ))}
+          </div>
+        </div>
+      )}
+      <p className="overflow-y-auto scrollbar-custom pr-[10px]">
+        {placeData.summary ?? "내용이 없습니다."}
+      </p>
+    </div>
   );
 };
 
