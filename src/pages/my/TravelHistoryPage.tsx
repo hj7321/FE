0fd205f0ccfea2d4fb09a7457b2c880425998af2
Hyeonjin <oboki2001@naver.com>
2025-06-09 02:39:48 +0900

    [refactor] TravelHistoryPage 구성 리팩터링 및 TravelHistorySection 컴포넌트 분리

--- src/pages/my/TravelHistoryPage.tsx
+++ src/pages/my/TravelHistoryPage.tsx
@@ -1,65 +1,20 @@
-import TravelPlanCard from "../../components/card/TravelPlanCard";
+import useReadTravelPlans from "../../hooks/useReadTravelPlans";
+import TravelHistorySection from "../../components/section/TravelHistorySection";
 
 const TravelHistoryPage = () => {
+  const { data: { allPlans, scheduledPlans, pastPlans } = {} } =
+    useReadTravelPlans();
+  console.log(allPlans);
+
   return (
-    <div className="px-[100px] py-[10px] flex flex-col">
-      <h1 className="font-bold text-[22px]">여행 기록 ✈️</h1>
-      <div className="mt-[7px] flex flex-wrap gap-x-[20px] gap-y-[15px]">
-        <TravelPlanCard
-          cardImg="/images/cities/나폴리.webp"
-          travelTitle="황금연휴에 가는 해외여행😊"
-          travelStartDate="2025.05.01"
-          travelEndDate="2025.05.07"
-          travelPeople={8}
-          travelPlace="스페인 바르셀로나"
-          isEnded={false}
-        />
-        <TravelPlanCard
-          cardImg="/images/cities/바르셀로나.webp"
-          travelTitle="황금연휴에 가는 해외여행😊"
-          travelStartDate="2025.05.01"
-          travelEndDate="2025.05.07"
-          travelPeople={8}
-          travelPlace="스페인 바르셀로나"
-          isEnded={true}
-        />
-        <TravelPlanCard
-          cardImg="/images/cities/뉴욕.webp"
-          travelTitle="황금연휴에 가는 해외여행😊"
-          travelStartDate="2025.05.01"
-          travelEndDate="2025.05.07"
-          travelPeople={8}
-          travelPlace="스페인 바르셀로나"
-          isEnded={false}
-        />
-        <TravelPlanCard
-          cardImg="/images/cities/도쿄.webp"
-          travelTitle="황금연휴에 가는 해외여행😊"
-          travelStartDate="2025.05.01"
-          travelEndDate="2025.05.07"
-          travelPeople={8}
-          travelPlace="스페인 바르셀로나"
-          isEnded={true}
-        />
-        <TravelPlanCard
-          cardImg="/images/cities/로마.webp"
-          travelTitle="황금연휴에 가는 해외여행😊"
-          travelStartDate="2025.05.01"
-          travelEndDate="2025.05.07"
-          travelPeople={8}
-          travelPlace="스페인 바르셀로나"
-          isEnded={true}
-        />
-        <TravelPlanCard
-          cardImg="/images/cities/로스앤젤레스.webp"
-          travelTitle="황금연휴에 가는 해외여행😊"
-          travelStartDate="2025.05.01"
-          travelEndDate="2025.05.07"
-          travelPeople={8}
-          travelPlace="스페인 바르셀로나"
-          isEnded={false}
-        />
-      </div>
+    <div className="px-[100px] py-[10px] flex flex-col gap-[5px]">
+      <TravelHistorySection
+        allPlans={allPlans ?? []}
+        scheduledPlans={scheduledPlans ?? []}
+        pastPlans={pastPlans ?? []}
+        showMyPageLink
+        linkToMyPage="/my"
+      />
     </div>
   );
 };
